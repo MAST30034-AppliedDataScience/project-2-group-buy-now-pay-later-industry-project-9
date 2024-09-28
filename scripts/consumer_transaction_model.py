4a0eb2b39b142576db601bb4c7e9d80b367b6e87
Alexq1n <sitaoq@student.unimelb.edu.au>
2024-09-28 14:53:35 +1000

    feature visualization

--- scripts/consumer_transaction_model.py
+++ scripts/consumer_transaction_model.py
@@ -1,12 +1,31 @@
-import holidays
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 
-def is_holiday(date):
-    """
-    Get all holidays in Australia in 2021
-    """
-    country = 'AU'
-    year = 2021
-    holiday_calendar = holidays.CountryHoliday(country, years=[year])
-    return date in holiday_calendar
+def feature_visualisation(df_pandas, plots):
+    # Set up a big plot grid
+    fig, axes = plt.subplots(6, 3, figsize=(20, 20))  # 4x3 grid of subplots
+    fig.tight_layout(pad=5.0)  # Adjust spacing between plots
+
+    for i, (plot_title, (feature, plot_type)) in enumerate(plots.items()):
+        ax = axes[i // 3, i % 3]  # Select subplot position
+
+        if plot_type == "hist":
+            sns.histplot(df_pandas[feature], bins=30, kde=True, ax=ax)
+        elif plot_type == "count":
+            sns.countplot(x=feature, data=df_pandas, ax=ax)
+        elif plot_type.startswith("scatter"):
+            if plot_type == "scatter1":
+                sns.scatterplot(x="Proportion_between_max_order_value_mean_income", y="fraud_probability", data=df_pandas, ax=ax)
+            elif plot_type == "scatter2":
+                sns.scatterplot(x="Proportion_between_max_order_value_median_income", y="fraud_probability", data=df_pandas, ax=ax)
+            elif plot_type == "scatter3":
+                sns.scatterplot(x="Proportion_between_total_order_value_mean_income", y="fraud_probability", data=df_pandas, ax=ax)
+            elif plot_type == "scatter4":
+                sns.scatterplot(x="Proportion_between_total_order_value_median_income", y="fraud_probability", data=df_pandas, ax=ax)
+
+        ax.set_title(plot_title)
+
+    # Show the entire plot
+    plt.show()
