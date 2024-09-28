e5e8c96c75c69fd779d488724dafbc52df58402b
Alexq1n <sitaoq@student.unimelb.edu.au>
2024-09-28 21:37:05 +1000

    log transformation and normalisation for old model

--- scripts/consumer_model.py
+++ scripts/consumer_model.py
@@ -7,6 +7,38 @@ from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
 from pyspark.ml import Pipeline
 from pyspark.ml.regression import LinearRegression
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+def feature_visualisation(df_pandas, plots):
+    # Set up a big plot grid
+    fig, axes = plt.subplots(6, 3, figsize=(20, 20))  # 4x3 grid of subplots
+    fig.tight_layout(pad=5.0)  # Adjust spacing between plots
+
+    for i, (plot_title, (feature, plot_type)) in enumerate(plots.items()):
+        ax = axes[i // 3, i % 3]  # Select subplot position
+
+        if plot_type == "hist":
+            sns.histplot(df_pandas[feature], bins=30, kde=True, ax=ax)
+        elif plot_type == "count":
+            sns.countplot(x=feature, data=df_pandas, ax=ax)
+        elif plot_type.startswith("scatter"):
+            if plot_type == "scatter1":
+                sns.scatterplot(x="Proportion_between_max_order_value_mean_income", y="average_fraud_probability", data=df_pandas, ax=ax)
+            elif plot_type == "scatter2":
+                sns.scatterplot(x="Proportion_between_max_order_value_median_income", y="average_fraud_probability", data=df_pandas, ax=ax)
+            elif plot_type == "scatter3":
+                sns.scatterplot(x="Proportion_between_total_order_value_mean_income", y="average_fraud_probability", data=df_pandas, ax=ax)
+            elif plot_type == "scatter4":
+                sns.scatterplot(x="Proportion_between_total_order_value_median_income", y="average_fraud_probability", data=df_pandas, ax=ax)
+
+        ax.set_title(plot_title)
+
+    # Show the entire plot
+    plt.show()
+
 
 
 
